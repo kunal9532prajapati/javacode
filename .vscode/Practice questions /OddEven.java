88bdc5e1fb4bad76a694f483497badf65515efca
kunal9532prajapati <155886094+kunal9532prajapati@users.noreply.github.com>
2025-08-11 23:34:50 +0530

    questions

---
+++ .vscode/Practice questions /OddEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class OddEven{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number to check Odd or Even : ");
+
+        int num = sc.nextInt();
+
+        if (num%2==0){
+            System.out.println("Number is Even: "+ num);
+
+        }else 
+            System.out.println("Number is Odd: "+ num);
+
+    }
+}
