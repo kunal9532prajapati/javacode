7f7d300d34e7e4d85fb1d4c369ebed6a29b95779
kunal9532prajapati <155886094+kunal9532prajapati@users.noreply.github.com>
2025-08-20 21:33:46 +0530

    commit

---
+++ Dsa_codes/PalindromeString.java
@@ -0,0 +1,24 @@
+public class PalindromeString {
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String testString = "racecar";
+        if (isPalindrome(testString)) {
+            System.out.println(testString + " is a palindrome.");
+        } else {
+            System.out.println(testString + " is not a palindrome.");
+        }
+    }
+}
