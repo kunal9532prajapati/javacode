9cc9ed8f66edd567a602d51f60d77145da62165c
kunal9532prajapati <155886094+kunal9532prajapati@users.noreply.github.com>
2025-08-29 23:08:55 +0530

    commit

--- Dsa_codes/Arrayss/RotateArray.java
+++ Dsa_codes/Arrayss/RotateArray.java
@@ -1,12 +1,35 @@
 package Arrayss;
 
 public class RotateArray {
-    public static void main(String[] args) {
-        static void RotateArr(int arr[], int d){
-            
-
+    public static void Reverse(int arr[], int i, int j){
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
         }
+    }
+    public static void Rotate(int arr[], int d){
+        int n = arr.length;
         
+        if(n==0) return;
+        d = d%n;// In case d > n
+        
+        Reverse(arr, 0, d-1);
+        Reverse(arr, d, n-1);
+        Reverse(arr, 0, n-1);
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int d = 13;
+        Rotate(arr, d);
+
+        System.out.print("Rotated Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
     
 }
--- Dsa_codes/Arrayss/missingElement.java
+++ Dsa_codes/Arrayss/missingElement.java
@@ -1,18 +1,33 @@
 package Arrayss;
 
-public class missingElement {
+// public class missingElement {
     
-    public static void main(String args[]){
-        int[] arr = {1,2,3,4,5,6};
-        int n = arr.length;
-        int sum = (n+1)*(n+2)/2;
-        int value=0;
+//     public static void main(String args[]){
+//         int[] arr = {1,2,3,4,5,6};
+//         int n = arr.length;
+//         int sum = (n+1)*(n+2)/2;
+//         int value=0;
 
-        for (int i= 0; i < n; i++){
-            value+= arr[i];
-        }
-        int num = sum-value;
-        System.out.println("this is the array missing element: "+ num);
+//         for (int i= 0; i < n; i++){
+//             value+= arr[i];
+//         }
+//         int num = sum-value;
+//         System.out.println("this is the array missing element: "+ num);
     
-    }   
+//     }   
+// }
+public class missingElement {
+    public static void main(String args[]) {
+        long[] arr = {1, 2, 3, 5, 6};
+        long n = arr.length + 1;               // total numbers should be 6
+        long sum = n * (n + 1) / 2;            // expected sum of 1..6
+        long arraySum = 0;
+
+        for (long num : arr) {
+            arraySum += num;
+        }
+
+        long missing = sum - arraySum;
+        System.out.println("This is the array missing element: " + missing);
+    }
 }
